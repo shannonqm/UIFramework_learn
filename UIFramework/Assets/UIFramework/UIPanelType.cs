a8cf9ff7040916362e6c1701a83ed07a62217ef2
DESKTOP-5ONF5IG\Administrator <shangqingmao@126.com>
2019-04-29 20:00:38 +0800

    制作好所有panel类型，和json格式

---
+++ UIFramework/Assets/UIFramework/UIPanelType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIPanelType {
+    Item,
+    Knapsack,
+    MainMenu,
+    Shop,
+    Skill,
+    System,
+    Task
+}
